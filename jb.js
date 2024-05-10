ce7193878f6e60f186852d513fc317311c11279e
Eden <52187529+ouoiouo@users.noreply.github.com>
2024-05-10 10:38:13 +0800

    Create jb.js

---
+++ jb.js
@@ -0,0 +1,9 @@
+{
+    "plugins": [
+        {
+            "name": "歌词千寻",
+            "url": "https://gitee.com/maotoumao/MusicFreePlugins/raw/v0.1/dist/geciqianxun/index.js",
+            "version": "0.0.0"
+        }
+    ]
+}
