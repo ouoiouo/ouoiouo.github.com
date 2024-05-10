efebdae84f198c5532bebe3bed0ce99edab39563
Eden <52187529+ouoiouo@users.noreply.github.com>
2024-05-10 10:41:11 +0800

    Update jb.js

--- jb.js
+++ jb.js
@@ -1,8 +1,28 @@
 {
     "plugins": [
         {
-            "name": "网易云",
+            "name": "WYY",
             "url": "https://gitee.com/ikun0014/musicfree/raw/master/dist/wy/index.js",
+            "version": "0.0.1"
+        },
+        {
+            "name": "QQ",
+            "url": "https://gitee.com/ikun0014/musicfree/raw/master/dist/qq/index.js",
+            "version": "0.0.1"
+        },
+        {
+            "name": "KG",
+            "url": "https://gitee.com/ikun0014/musicfree/raw/master/dist/kg/index.js",
+            "version": "0.0.1"
+        },
+        {
+            "name": "KW",
+            "url": "https://gitee.com/ikun0014/musicfree/raw/master/dist/kw/index.js",
+            "version": "0.0.1"
+        },
+        {
+            "name": "MG",
+            "url": "https://gitee.com/ikun0014/musicfree/raw/master/dist/mg/index.js",
             "version": "0.0.1"
         }
     ]
